7d1173a810c5aecb105be44accba8bb2a9a8cc21
ameymj <amey.joshi0929@gmail.com>
2022-09-09 18:48:56 +0530

    about us and services page updated

--- FrontEnd/src/pages/About/About.js
+++ FrontEnd/src/pages/About/About.js
@@ -7,15 +7,29 @@ import trusted from './trusted.jpeg'
 
 function About() {
   return (
+    
     <div className='about'>
-      <h1 style={{textAlign:'center'}}>Our Policies</h1>
+      <h1 style={{textAlign:'center'}}>What is sharesteer</h1>
+      <h3>Share your empty car seats and reduce your travelling costs. 
+      meet new people every day in on your route, grow your network and make your rides interesting
+      </h3>
+      
+      <h5>
+      We all like going together. 
+      Share-Steer will help you to connect with other professionals who are traveling on the same route
+      and same time. Share-Steer helps  to start sharing the ride instead of traveling alone. 
+      Best sit to discover, connect, route match, coordinate and cost-share.
+      Share-Steer makes carpooling convnient, safe, 
+      comfortable and affordable for the urban commuters
+      </h5>
     <CardGroup>
       <Card>
         <Card.Img variant="top" src={safe} />
         <Card.Body>
           <Card.Title>Travel Safe</Card.Title>
           <Card.Text>
-          Yaha Bhi Kuchh Likhna Hai
+            Check the profiles of your co travellers.
+            We verify profile and also check reviews and ratings.
           </Card.Text>
         </Card.Body>
         <Card.Footer>
@@ -27,7 +41,8 @@ function About() {
         <Card.Body>
           <Card.Title>Ontime Boarding</Card.Title>
           <Card.Text>
-          Yaha Bhi Kuchh Likhna Hai
+          Reviews and ratings according to puncutality of co-traveller.
+          Its better to be soon than a minute late.
           </Card.Text>
         </Card.Body>
         <Card.Footer>
@@ -39,7 +54,10 @@ function About() {
         <Card.Body>
           <Card.Title>Collaboration</Card.Title>
           <Card.Text>
-            Yaha Bhi Kuchh Likhna Hai
+            Real time ride sharing app.
+            Car Sharing reduces trafic.
+            Reduces carbon immissions and
+            also reduces the fuel cost.
           </Card.Text>
         </Card.Body>
         <Card.Footer>
@@ -48,6 +66,7 @@ function About() {
       </Card>
     </CardGroup>
     </div>
+ 
   );
 }
 
--- FrontEnd/src/pages/Service/Services.js
+++ FrontEnd/src/pages/Service/Services.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Services.css'
 const Services = () => {
   return (
+    
     <div className="container">
     <section>
       <div class="row">
@@ -15,7 +16,9 @@ const Services = () => {
             </div>
             <h3>Trusted Drivers</h3>
             <p>
-              Write Something Here
+              We check the reviews and feedback of the drivers.
+              We verify The driving licence of driver.
+              We also verify photo-id of the treavelers.
             </p>
           </div>
         </div>
@@ -26,7 +29,8 @@ const Services = () => {
             </div>
             <h3>Emergency Service</h3>
             <p>
-            Write Something Here
+            We have colaboration with SOS for highway-Safety.
+            contact our toll free number 1001001000 in case of emergency.
             </p>
           </div>
         </div>
@@ -37,7 +41,7 @@ const Services = () => {
             </div>
             <h3>Maintained Vehicle</h3>
             <p>
-            Write Something Here
+            Check the photoes of the vehicle with wich you are travelling.
             </p>
           </div>
         </div>
@@ -48,7 +52,7 @@ const Services = () => {
             </div>
             <h3>Live Tracking</h3>
             <p>
-            Write Something Here
+            Track the live location of the vehile as soon as the driver starts the journey.
             </p>
           </div>
         </div>
@@ -59,7 +63,8 @@ const Services = () => {
             </div>
             <h3>Feedback System</h3>
             <p>
-            Write Something Here
+            your feedback are important to us and other users.
+            Share your overall experience and experience with perticular user.
             </p>
           </div>
         </div>
@@ -70,7 +75,7 @@ const Services = () => {
             </div>
             <h3>Ontime </h3>
             <p>
-            Write Something Here
+            You can write us if the driver delays or cancels the ride on services@sharesteer.com.
             </p>
           </div>
         </div>
