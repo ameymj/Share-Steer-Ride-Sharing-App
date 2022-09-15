896534b306e67800ca58bd6cb973caf089159c6f
ameymj <amey.joshi0929@gmail.com>
2022-09-15 19:58:37 +0530

    Review component updated and images added

--- FrontEnd/src/pages/Review/Review.js
+++ FrontEnd/src/pages/Review/Review.js
@@ -1,7 +1,23 @@
+import { useEffect, useState } from 'react';
+import Rating from './rating.jpg';
+import Reviews from './review.jpg';
+import { useNavigate } from 'react-router';
+import axios from 'axios';
 
+import { ReactSession } from 'react-client-session';
 
 export default function Review()
 {
+
+    const [rating, setRating] = useState("");
+    const [review, setReview] = useState("");
+    const [messege, setMesssge] = useState("");
+
+    function verify() {
+
+    }
+
+    return(
     <section>
             <div className="container py-5 h-100">
                 <div className="row d-flex justify-content-center align-items-center h-100">
@@ -9,10 +25,10 @@ export default function Review()
                         <div className="card" style={{ "borderRadius": " 1rem" }}>
                             <div className="row g-0">
                                 <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                    <img src={Welcome}
-                                        alt="login form" className="img-fluid" style={{ "borderRadius": " 1rem 0 0 1rem" }} /><br /><br /><br />
-                                    <img src={Welcom}
-                                        alt="login form" className="img-fluid" style={{ "borderRadius": " 1rem 0 0 1rem" }} />
+                                    <img src={Reviews}
+                                        alt="reviewimg" className="img-fluid" style={{ "borderRadius": " 1rem 0 0 1rem" }} /><br /><br /><br />
+                                    <img src={Rating}
+                                        alt="ratingimg" className="img-fluid" style={{ "borderRadius": " 1rem 0 0 1rem" }} />
                                 </div>
                                 <div className="col-md-6 col-lg-7 d-flex align-items-center">
                                     <div className="card-body p-4 p-lg-5 text-black" style={{ 'backgroundColor': 'rgb(5, 101, 134)' }}>
@@ -25,12 +41,18 @@ export default function Review()
 
 
                                             <div className="form-outline mb-4">
-                                                <input type="number" id="form2Example17" className="form-control form-control-lg" required minLength={8} maxLength={16} onBlur={(e) => { setusername(e.target.value) }} />
-                                                <label className="form-label">Rate out of 5</label>
+                                            <select className="form-select btn btn-dark btn-lg btn-block" aria-label="Default select example" onBlur={(e) => { setRating(e.target.value) }}>
+                                                        <option value="1">*</option>
+                                                        <option value="2">* *</option>
+                                                        <option value="3">* * *</option>
+                                                        <option value="4">* * * *</option>
+                                                        <option value="5">* * * * *</option>
+                                                    </select>
+                                                <label className="form-label">Rate out of 5 </label>
                                             </div>
 
                                             <div className="form-outline mb-4">
-                                                <input type="text" id="form2Example27" className="form-control form-control-lg" required minLength={8} maxLength={16} onBlur={(e) => { setPassword(e.target.value) }} />
+                                                <input type="text" id="form2Example27" className="form-control form-control-lg"  onBlur={(e) => { setReview(e.target.value) }} />
                                                 <label className="form-label">Write comment</label>
                                             </div>
 
@@ -48,4 +70,5 @@ export default function Review()
                 </div>
             </div>
         </section>
+        );
 }
