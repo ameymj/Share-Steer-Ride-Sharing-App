0166cc9c5f123c424c0f06d7c314a2454e5e2db8
swapnilkalange <106860970+swapnilkalange@users.noreply.github.com>
2022-09-16 13:32:48 +0530

    Update Testimonial component
    Update component with basic expresssion by our testimonials and experts

--- FrontEnd/src/pages/Testimonial/Testimonial.js
+++ FrontEnd/src/pages/Testimonial/Testimonial.js
@@ -19,9 +19,9 @@ export default class Testimonials extends Component {
     <div className="col-md-10 col-xl-8 text-center">
       <h3 className="mb-4">Testimonials</h3>
       <p className="mb-4 pb-2 mb-md-5 pb-md-0">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet
-        numquam iure provident voluptate esse quasi, veritatis totam voluptas nostrum
-        quisquam eum porro a pariatur veniam.
+        A testimonial is an honest endorsement of your product or service that usually comes 
+        from a customer, colleague, or peer who has benefited from or experienced success as a 
+        result of the work you did for them.
       </p>
     </div>
   </div>
@@ -111,15 +111,15 @@ export default class Testimonials extends Component {
   <div className="row text-center">
     <div className="col-md-4 mb-5 mb-md-0">
       <div className="d-flex justify-content-center mb-4">
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).webp"
+        <img src={Amey}
           className="rounded-circle shadow-1-strong" width={"150"} height={"150"} />
       </div>
-      <h5 className="mb-3">Maria Smantha</h5>
+      <h5 className="mb-3">Amey Joshi</h5>
       <h6 className="text-primary mb-3">Web Developer</h6>
       <p className="px-xl-3">
-        <i className="fas fa-quote-left pe-2"></i>Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit. Quod eos id officiis hic tenetur quae quaerat ad velit ab hic
-        tenetur.
+        <i className="fas fa-quote-left pe-2"></i>
+Through carpooling, you will get different options to commute, which you will not get through other modes of transportation. 
+This service is bliss for people who have negligible access to other methods.
       </p>
       <ul className="list-unstyled d-flex justify-content-center mb-0">
         <li>
@@ -141,14 +141,14 @@ export default class Testimonials extends Component {
     </div>
     <div className="col-md-4 mb-5 mb-md-0">
       <div className="d-flex justify-content-center mb-4">
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp"
+        <img src={Naman}
           className="rounded-circle shadow-1-strong" width={"150"} height={"150"}  />
       </div>
-      <h5 className="mb-3">Lisa Cudrow</h5>
+      <h5 className="mb-3">Naman Saxena</h5>
       <h6 className="text-primary mb-3">Graphic Designer</h6>
       <p className="px-xl-3">
-        <i className="fas fa-quote-left pe-2"></i>Ut enim ad minima veniam, quis nostrum
-        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid commodi.
+        <i className="fas fa-quote-left pe-2"></i>While commuting with the same set of people regularly, you will tend to bond with them, and thus,
+you will end up making friends.
       </p>
       <ul className="list-unstyled d-flex justify-content-center mb-0">
         <li>
@@ -170,14 +170,13 @@ export default class Testimonials extends Component {
     </div>
     <div className="col-md-4 mb-0">
       <div className="d-flex justify-content-center mb-4">
-        <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(9).webp"
+        <img src={Mahesh}
           className="rounded-circle shadow-1-strong" width={"150"} height={"150"}  />
       </div>
-      <h5 className="mb-3">John Smith</h5>
+      <h5 className="mb-3">Mahesh Shinde</h5>
       <h6 className="text-primary mb-3">Marketing Specialist</h6>
       <p className="px-xl-3">
-        <i className="fas fa-quote-left pe-2"></i>At vero eos et accusamus et iusto odio
-        dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti.
+        <i className="fas fa-quote-left pe-2"></i>This Is the Best web application I have wondered in the ride sharing Industry.
       </p>
       <ul className="list-unstyled d-flex justify-content-center mb-0">
         <li>
