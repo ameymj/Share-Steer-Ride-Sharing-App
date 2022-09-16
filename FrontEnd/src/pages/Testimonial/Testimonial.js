e3302c637f42adf55e67e9029a65965f7a71bd60
Tushar Ravindra Sthul <101002171+PureCoderTush@users.noreply.github.com>
2022-09-16 09:46:06 +0530

    Update Testinonial page
    Add header Paragraph for testimonial page.

--- FrontEnd/src/pages/Testimonial/Testimonial.js
+++ FrontEnd/src/pages/Testimonial/Testimonial.js
@@ -19,9 +19,9 @@ export default class Testimonials extends Component {
     <div className="col-md-10 col-xl-8 text-center">
       <h3 className="mb-4">Testimonials</h3>
       <p className="mb-4 pb-2 mb-md-5 pb-md-0">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet
-        numquam iure provident voluptate esse quasi, veritatis totam voluptas nostrum
-        quisquam eum porro a pariatur veniam.
+        A testimonial is an honest endorsement of your product or service that usually comes 
+        from a customer, colleague, or peer who has benefited from or experienced success as a 
+        result of the work you did for them.
       </p>
     </div>
   </div>
@@ -114,7 +114,7 @@ export default class Testimonials extends Component {
         <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(1).webp"
           className="rounded-circle shadow-1-strong" width={"150"} height={"150"} />
       </div>
-      <h5 className="mb-3">Maria Smantha</h5>
+      <h5 className="mb-3">Amey Joshi</h5>
       <h6 className="text-primary mb-3">Web Developer</h6>
       <p className="px-xl-3">
         <i className="fas fa-quote-left pe-2"></i>Lorem ipsum dolor sit amet, consectetur
@@ -144,7 +144,7 @@ export default class Testimonials extends Component {
         <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(2).webp"
           className="rounded-circle shadow-1-strong" width={"150"} height={"150"}  />
       </div>
-      <h5 className="mb-3">Lisa Cudrow</h5>
+      <h5 className="mb-3">Naman Saxena</h5>
       <h6 className="text-primary mb-3">Graphic Designer</h6>
       <p className="px-xl-3">
         <i className="fas fa-quote-left pe-2"></i>Ut enim ad minima veniam, quis nostrum
@@ -173,7 +173,7 @@ export default class Testimonials extends Component {
         <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(9).webp"
           className="rounded-circle shadow-1-strong" width={"150"} height={"150"}  />
       </div>
-      <h5 className="mb-3">John Smith</h5>
+      <h5 className="mb-3">Mahesh Shinde</h5>
       <h6 className="text-primary mb-3">Marketing Specialist</h6>
       <p className="px-xl-3">
         <i className="fas fa-quote-left pe-2"></i>At vero eos et accusamus et iusto odio
